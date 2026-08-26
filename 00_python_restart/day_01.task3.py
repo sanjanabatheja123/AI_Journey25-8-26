18dc8f8437ee01f298a11671f0e633b45cc809af
sanjanabatheja123 <sanjanabatheja123@gmail.com>
2026-08-26 09:33:45 +0530

    practice input with var

--- 00_python_restart/day_01.task3.py
+++ 00_python_restart/day_01.task3.py
@@ -2,3 +2,4 @@ name = input("what's your name?: ")
 age = int(input("please enter your age: "))
 
 print(f"Hello {name}, you are {age} years old.")
+print(f"Next yea you will be {age + 1 } years old.")
